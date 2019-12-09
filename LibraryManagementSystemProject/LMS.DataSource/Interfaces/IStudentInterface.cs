3ae3562dbcf228e683cfc9663ec6ff828105f3fe
IreshaSilva96 <57743712+IreshaSilva96@users.noreply.github.com>
2019-12-09 19:51:22 +0530

    Progress on student repository

---
+++ LibraryManagementSystemProject/LMS.DataSource/Interfaces/IStudentInterface.cs
@@ -0,0 +1,25 @@
+﻿using LMS.DataSource.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.DataSource.Interfaces
+{
+    public interface IStudentInterface
+    {
+        ICollection<Student>GetAllStudents();
+
+        Student GetStudentByID(int ID);
+
+        Student GetStudentByAttribute(int ID);
+
+        ICollection<Borrowing> GetStudentID_ByBorrowings(int ID);
+
+        ICollection<Payment> GetStudentID_ByPayments(int ID);
+
+        int BlockStudent(int id, Student studentObject);
+
+        int ResetPassword(int id, Student studentObject);
+
+    }
+}
---
+++ LibraryManagementSystemProject/LMS.DataSource/Repositories/StudentRepository.cs
@@ -0,0 +1,53 @@
+﻿using LMS.DataSource.Entities;
+using LMS.DataSource.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.DataSource.Repositories
+{
+    public class StudentRepository : IStudentInterface
+    {
+        AppDbContext appDbContext;
+
+        public StudentRepository(AppDbContext dbContext)
+        {
+            appDbContext = dbContext;
+        }
+        
+        public int BlockStudent(int id, Student studentObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Student> GetAllStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetStudentByAttribute(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetStudentByID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Borrowing> GetStudentID_ByBorrowings(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Payment> GetStudentID_ByPayments(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ResetPassword(int id, Student studentObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
